81df8d260e6522069962b5dfd4b3bcd0be8cec2f
sakumonk <67976136+sakumonk@users.noreply.github.com>
2021-12-10 16:37:31 -0500

    produce dataset with 5 more individuals (25 more images)

--- DifferentFormatImages.py
+++ DifferentFormatImages.py
@@ -4,15 +4,33 @@ import os
 from imageprocessing_helper_functions import flatten
 from sklearn.model_selection import train_test_split
 
-#images convert to grayscale and resized
-images = [f for f in os.listdir('obama') if f != ".DS_Store"]
-X = [Image.open("./obama/" + image) for image in images]
-for i in range(0, len(X)):
-    X[i] = ImageOps.grayscale(X[i])
-    X[i] = X[i].resize((243,320))
+
+def FiveMoreImages():
+
+    #images convert to grayscale and resized
+    images = [f for f in os.listdir('Augmented_With_5_More') if f != ".DS_Store"]
+    X = [Image.open("./Augmented_With_5_More/" + image) for image in images]
+    for i in range(0, len(X)):
+        X[i] = ImageOps.grayscale(X[i])
+        X[i] = X[i].resize((243,320))
 
 
-for image in X:
-    print(image)
-    image.show()
 
+    label = []
+    for image in images:
+        if "putin" in image: #subject 16 is Putin
+            label.append(16)
+        elif "obama" in image: #subject 17 is Obama
+            label.append(17)
+        elif "prayuth" in image: #subject 18 is General Prayuth
+            label.append(18)
+        elif "Depp" in image: #subject 19 is Johnny Depp
+            label.append(19)
+        elif "kim" in image: #subject 20 is Kim Jong Un
+            label.append(20)
+
+    label = np.array(label)
+    features = flatten(X)
+    return features, label
+
+FiveMoreImages()
---
+++ imagesToNPYFiveMoreImages.py
@@ -0,0 +1,66 @@
+import numpy as np
+from PIL import Image
+import os
+from imageprocessing_helper_functions import flatten
+from sklearn.model_selection import train_test_split
+from DifferentFormatImages import FiveMoreImages
+
+#yale images
+images = [f for f in os.listdir('yalefaces') if f != ".DS_Store"]
+X = [Image.open("./yalefaces/" + image) for image in images]
+
+#divide into test,dev, and train (test, train, dev)
+label = []
+for image in images:
+    if "subject11" in image:
+        label.append(11)
+    elif "subject02" in image:
+        label.append(2)
+    elif "subject03" in image:
+        label.append(3)
+    elif "subject04" in image:
+        label.append(4)
+    elif "subject05" in image:
+        label.append(5)
+    elif "subject06" in image:
+        label.append(6)
+    elif "subject07" in image:
+        label.append(7)
+    elif "subject08" in image:
+        label.append(8)
+    elif "subject09" in image:
+        label.append(9)
+    elif "subject10" in image:
+        label.append(10)
+    elif "subject15" in image:
+        label.append(15)
+    elif "subject12" in image:
+        label.append(12)
+    elif "subject13" in image:
+        label.append(13)
+    elif "subject14" in image:
+        label.append(14)
+    elif "subject01" in image:
+        label.append(1)
+
+label = np.array(label)
+features = flatten(X)
+
+features_fivemore, label_fivemore = FiveMoreImages()
+
+#combined
+features_combined = np.concatenate((features, features_fivemore))
+labels_combined = np.concatenate((label, label_fivemore))
+
+
+train_features, dev_features, train_labels, dev_labels = train_test_split(features_combined, labels_combined, test_size=0.4, random_state=42)
+dev_features, test_features, dev_labels, test_labels = train_test_split(dev_features, dev_labels, test_size=0.1, random_state=42)
+
+
+
+np.save(os.path.join("yalefaces_npy_fivemore","dev.feats"), dev_features)
+np.save(os.path.join("yalefaces_npy_fivemore","test.feats"), test_features)
+np.save(os.path.join("yalefaces_npy_fivemore","train.feats"), train_features)
+np.save(os.path.join("yalefaces_npy_fivemore","dev.labels"), dev_labels)
+np.save(os.path.join("yalefaces_npy_fivemore","test.labels"), test_labels)
+np.save(os.path.join("yalefaces_npy_fivemore","train.labels"), train_labels)
